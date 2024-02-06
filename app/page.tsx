06de4057196901a69f8e1873b174af3a0b603da0
FatsaYahdi <namadepannamabelakang10298318@gmail.com>
2024-02-06 19:12:14 +0700

    add framer motion & trying Aceternity UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+import { useReportWebVitals } from "next/web-vitals";
+import { Meteors } from "@/components/common/meteors";
+import { TextGenerateEffect } from "@/components/common/text-generate-effect";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 
 export default function Home() {
+  useReportWebVitals((metric) => {
+    console.log(metric);
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -11,9 +18,9 @@ export default function Home() {
           <ThemeSwitcher />
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        hi
+      <Meteors number={40} />
+      <div className="relative flex flex-col">
+        <TextGenerateEffect words="Welcome" />
       </div>
       <div></div>
     </main>
