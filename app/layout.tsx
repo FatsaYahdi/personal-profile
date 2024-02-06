fefdc1a5d949828cf21bf743aa8b2ebe13f6a77f
FatsaYahdi <namadepannamabelakang10298318@gmail.com>
2024-02-06 19:24:02 +0700

    Revert "add vercel insight and next theme"
    This reverts commit 6b6e309d7f1d948d0b888c375793b04b15669863.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,16 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem={true}
-        >
-          {children}
-          <SpeedInsights />
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
