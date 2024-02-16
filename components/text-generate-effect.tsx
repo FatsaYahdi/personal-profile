f43c5a81501f5e5a931a5a807375357f014a36e6
FatsaYahdi <namadepannamabelakang10298318@gmail.com>
2024-02-16 19:09:00 +0700

    add text generate effect

---
+++ components/text-generate-effect.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const TextGenerateEffect = ({
+  words,
+  className,
+}: {
+  words: string;
+  className?: string;
+}) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(0.2),
+      }
+    );
+  }, [animate]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope} className={cn("", className)}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span key={word + idx} className="opacity-0">
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return <>{renderWords()}</>;
+};
