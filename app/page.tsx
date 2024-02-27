3016db81d76b49c708dcc738f70609f9a113cda0
FatsaYahdi <namadepannamabelakang10298318@gmail.com>
2024-02-27 14:44:03 +0700

    fix missing background

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
             </div>
           </Link>
         </div>
-        {/* <BackgroundBeams className="opacity-100" /> */}
+        <BackgroundBeams className="opacity-100" />
       </main>
       <main className="flex lg:hidden h-screen items-center justify-center flex-col">
         <h1 className="text-4xl  font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
