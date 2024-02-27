5d248a8279ea7a677b91c581c45692855641dd56
FatsaYahdi <namadepannamabelakang10298318@gmail.com>
2024-02-27 14:42:25 +0700

    fix theme switcher font size

--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -24,14 +24,23 @@ export function ThemeSwitcher() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+      <DropdownMenuContent align="center">
+        <DropdownMenuItem
+          className="text-lg font-fredoka"
+          onClick={() => setTheme("light")}
+        >
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem
+          className="text-lg font-fredoka"
+          onClick={() => setTheme("dark")}
+        >
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem
+          className="text-lg font-fredoka"
+          onClick={() => setTheme("system")}
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
